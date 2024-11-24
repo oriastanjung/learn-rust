6bd9fe935591792c66a25a186ab17442f20ce7f3
oriastanjung <oriastan999@gmail.com>
2024-11-24 21:27:38 +0700

    learn variable

--- src/main.rs
+++ src/main.rs
@@ -2,3 +2,16 @@ fn main() {
     let message = "Hello, dunia!";
     println!("{}", message);
 }
+
+#[test]
+fn learn_variable() {
+//    immutable
+    let x = 5;
+//    mutable
+    let mut var1 = 21;
+    println!("{}",var1);
+    var1 = 5;
+    println!("{}",var1);
+    var1 = 6;
+    println!("x: {}, var2: {}", x, var1);
+}
